94476f3e71a6e6f78bf3b79288973ec2e99cc72d
VasiliyRom <63020273+VasiliyRom@users.noreply.github.com>
2020-04-02 20:33:47 +0300

    Update rom.py

---
+++ rom.py
@@ -0,0 +1,60 @@
+# Перший коментар
+int_value = 1 # Ціле число, integer or int
+neg_int_value = -1 # Ціле відємне число, integer or int
+print(int_value)
+print(neg_int_value)
+
+float_value =1.234 # float, дробове число або число з плаваючою точкою
+print(float_value)
+
+string_value = 'text1' # string or str стрічка
+print(string_value)
+
+string_second = "text2"
+print(string_second)
+
+
+
+negativ_statement = False # це нуль( 0 )
+positiv_statement = True # це ( 1 )
+
+print(negativ_statement)
+print(positiv_statement)
+
+
+print()
+
+x = 2
+y = 2
+
+print(x + y)
+print(x - y)
+print(x / y)
+print(x * y)
+
+print(x % y)
+print(x // y)
+
+print(f'Sum: {x} + {y} = {x+y}')
+print(f'Ext: {x} - {y} = {x-y}')
+print(f'Div: {x} / {y} = {x/y}')
+print(f'Mult: {x} * {y} = {x*y}')
+print(f'Modulo: {x} mod {y} = {x%y}')
+
+x=2
+y=3
+Comparison_resutl = x == y
+print('Comparing {x} and {y}: {Comparison_resutl}')
+print(f'Is {x} more than {y}: {x>y}')
+
+print(f'Is {x} more than {y}: {x<y}')
+
+print(f'Is {x} more or equal than {y}: {x>=y}')
+
+print(f'Is {x} more or equal than {y}: {x>=y}')
+
+
+
+
+
+
