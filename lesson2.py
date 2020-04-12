3518331435a98abbeed93a5751e561ba77118a3d
VasiliyRom <63020273+VasiliyRom@users.noreply.github.com>
2020-04-12 22:42:34 +0300

    Update lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -1,12 +1,15 @@
+# task1
 name = "Василь"
 day = " Неділя"
 print ("Добрий день", name, end='!')
 print (day, "- ідеальний день для вивчення петону")
 
+# task2
 name = "Василь" 
 name += " Романюк"
 print (name)
 
+# task3
 a, b = 3.2, 5.6
 print(a+b)
 print(a-b)
@@ -15,8 +18,3 @@ print(a*b)
 print(a**b)
 print(a//b)
 print(a%b)
-
-
-name = ‘Dostoyevsky’
-lenght_of_name = len(name)
-print(lenght_of_name)
