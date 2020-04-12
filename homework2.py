b0fb88e6ea24e5a2936343b53da0bfddb4bce8b1
VasiliyRom <63020273+VasiliyRom@users.noreply.github.com>
2020-04-12 15:10:02 +0300

    Create homework2.py

---
+++ homework2.py
@@ -0,0 +1,22 @@
+name = "Василь"
+day = " Неділя"
+print ("Добрий день", name, end='!')
+print (day, "- ідеальний день для вивчення петону")
+
+name = "Василь" 
+name += " Романюк"
+print (name)
+
+a, b = 3.2, 5.6
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a**b)
+print(a//b)
+print(a%b)
+
+
+name = ‘Dostoyevsky’
+lenght_of_name = len(name)
+print(lenght_of_name)
