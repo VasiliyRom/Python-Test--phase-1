902e040024b49770ba874ba51b3d1719950b473c
VasiliyRom <63020273+VasiliyRom@users.noreply.github.com>
2020-04-12 13:48:39 +0300

    Create task 1.py

---
+++ task 1.py
@@ -0,0 +1,38 @@
+# ‘hello world’
+print("hello world")
+
+print("Hallo, world")
+
+print('First Line', end=' ')
+print('Second Line', end='\n\n\n')
+print('Third Line', end='!')
+
+print('Third Line', end='\t')
+print('Third Line')
+
+print('#########')
+print('#', end='\t')
+print('#')
+print('#', end='\t')
+print('#')
+print('#', end='\t')
+print('#')
+print('#', end='\t')
+print('#')
+print('#', end='\t')
+print('#')
+print('#########')
+
+print('#', end='\t')
+print('#')
+print('#', end='\t')
+print('#')
+print('#', end='\t')
+print('#')
+print('#########')
+print('#', end='\t')
+print('#')
+print('#', end='\t')
+print('#')
+print('#', end='\t')
+print('#')
