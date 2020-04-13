2eeb94b43882209b80b96afd87d3a0a390b26ade
VasiliyRom <63020273+VasiliyRom@users.noreply.github.com>
2020-04-13 22:25:39 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,12 +1,22 @@
+# Task 1
 name = "Василь"
-day = " Неділя"
-print ("Добрий день", name, end='!')
-print (day, "- ідеальний день для вивчення петону")
+day = "Неділя"
+print(f'Добрий день {name}! {day} - ідеальний день для вивчення петону')
 
-name = "Василь" 
-name += " Романюк"
-print (name)
+# Task 2
+name = "Василь"
+last_name = "Романюк"
+print(f'Привіт {name} {last_name}')
 
+
+pattern = '{msg} {name} {last_name}'
+print(pattern.format(name='Василь', last_name='Романюк', msg='Привіт'))
+
+
+pattern = '{Романюк}, {Василь}, {Привіт}'
+print(pattern.format(1,2))
+
+# Task 3
 a, b = 3.2, 5.6
 print(a+b)
 print(a-b)
