3b642dfdfde09ce5cb6f818645c3d656bb43a334
VasiliyRom <63020273+VasiliyRom@users.noreply.github.com>
2020-04-24 16:48:36 +0300

    Create homework5.py

---
+++ homework5.py
@@ -0,0 +1,65 @@
+#Task1
+import random
+result = []
+while len(result) != 10: 
+    numbers = random.randint(1,6892)
+    result.append(numbers)
+print(result)
+print(max(result))
+
+
+#Task2
+import random
+collection_1 = []
+collection_2 = []
+collection_3 = []
+result = []
+while len(collection_1) != 10 and (collection_2) != 10: 
+    numbers = random.randint(1,10)
+    collection_1.append(numbers)
+    numbers1 = random.randint(1,10)
+    collection_2.append(numbers1)
+
+collection_3 = collection_1 + collection_2
+result = list(set(collection_3))
+print('Результат -', result)
+
+
+
+
+#Task3
+a = []
+b = []
+i = 1
+while True:
+    a.append(i)
+    i += 1
+    if i == 101:
+        break
+print(a)
+print()
+x=0
+while True:
+    if x in a and x & 7 == 0 and x % 5 != 0:
+        b.append(x)
+        x += 1
+print(b)
+
+
+
+
+##Task3 FOR
+a = []
+b = []
+i = 1
+while True:
+    a.append(i)
+    i += 1
+    if i == 101:
+        break
+print(a)
+print()
+for x in a:
+    if x % 7 == 0 and x % 5 != 0:
+        b.append(x)
+print(b) 
