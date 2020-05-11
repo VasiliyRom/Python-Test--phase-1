fba9fb33ccca0cbe346430974d8d4605d9abefad
VasiliyRom <63020273+VasiliyRom@users.noreply.github.com>
2020-05-12 00:10:34 +0300

    1

--- 1234.py
+++ 1234.py
@@ -1,121 +1,158 @@
 import json
-        
+'''import sys
+
+print(sys.argv)
+if len(sys.argv) < 2:
+    print('Будь ласка виберіть json файл')
+    exit()
+filename = sys.argv[1]
+json_file = open(filename)
+
+'''
+
+
 def new_contact(first_name, last_name, full_name, number, city):
-    user_info = {"phonebook": "name",
-    "first_name": first_name,
+    user_info = {"first_name": first_name,
     "last_name": last_name,
     "full_name": full_name,
     "number": number,
     "city": city
 }
-    try:
-        phonebook = json.load(open('phonebook.json'))
-    except:
-        phonebook = []
+    #try:
+    #    phonebook = json.load(open('phonebook.json'))
+    #except:
+    #    phonebook = []
 
     phonebook.append(user_info)
 
     with open('phonebook.json', 'w') as filee:
         json.dump(phonebook, filee, indent=4, ensure_ascii=False)
+    print('Ok')
+
+def remove(item):
+    phonebook.remove(item)
+    with open('phonebook.json', 'w') as filee:
+        json.dump(phonebook, filee, indent=4, ensure_ascii=False)
     return print('Ok')
 
+def search_con(query, arg, phonebook):
+    for item in phonebook:
+        if query == item[arg]:
+            print('\nІм\'я: ' + str(item['first_name']))
+            print('Прізвище: ' + str(item['last_name']))
+            print('Повне iм\'я: ' + str(item['full_name']))
+            print('Номер тел: ' + str(item['number']))
+            print('Місто: ' + str(item['city'] + '\n'))
+        else:
+            print('Запису неіснує!')
 
 
 print('''
     Додати запис_____________________________(1)
-    Пошук за імені___________________________(2)
-    Пошук за прізвищу________________________(3)
-    Пошук за повним іменем___________________(4)
-    Пошук за номером телефону________________(5)
-    Пошук по місту___________________________(6)
-    Видаліть запис для номера телефону_______(7)
-    Оновіть запис для номеру телефону________(8)
-    Вийти з програми_________________________(q)\n''')
+    Пошук____________________________________(2)
+    Видаліть запис для номера телефону_______(3)
+    Оновіть запис для номеру телефону________(4)
+    Вийти з програми_________________________(exit, q)\n''')
 
 while True:
-    operator = input('Введіть операцію: ')
+    operator = input('Головне меню: ').strip().lower()
     try:
         phonebook = json.load(open('phonebook.json'))
+        
     except:
-        print('Телефонна книга незнайдена!')
-        break
-
+        phonebook = []
+       
     if operator == '1':
-        first_name = input('Введіть ім\'я: ').capitalize()
-        last_name = input('Введіть Прізвище: ').capitalize()
+        first_name = input('Введіть ім\'я: ').strip().capitalize()
+        last_name = input('Введіть Прізвище: ').strip().capitalize()
         full_name = (f'{first_name} {last_name}')
-        number = input('Введіть номер тел: ')
-        city = input('Введіть місто: ').capitalize()
+        number_alp = input('Введіть номер тел: ').strip()
+        while True:
+            if number_alp.isdigit():
+                number = number_alp
+                break
+            else:
+                print('Номер має містити тільки цифри!')
+                number_alp = input('Введіть номер тел: ').strip()
+                
+        city = input('Введіть місто: ').strip().capitalize()
 
         new_contact(first_name, last_name, full_name, number, city)
         
 
     elif operator == '2':
-        query = input('Введіть ім\'я: ')
-        for item in phonebook:
-            if query in item['first_name']:
-                print(item)
-                continue
+        print('''Пошук за імені___________________________(1)
+Пошук за прізвищу________________________(2)
+Пошук за повним іменем___________________(3)
+Пошук за номером телефону________________(4)
+Пошук по місту___________________________(5)
+Назад в головне меню_____________________(6)\n''')
+        search = input('Пошук: ')
+        if search == '1':
+            query = input('Введіть ім\'я: ').strip().capitalize()
+            atr = 'first_name'
+            search_con(query, atr, phonebook) 
+            
+        elif search == '2':
+            query = input('Введіть прізвище: ').strip().capitalize()
+            atr = 'last_name'
+            search_con(query, atr, phonebook)
+        elif search == '3':
+            query = input('Введіть повне ім\'я: ').capitalize()
+            atr = 'full_name'
+            search_con(query, atr, phonebook)
+        elif search == '4':
+            query = input('Введіть номер телефону: ').strip()
+            if query.isalpha():
+                print('Номер має містити тільки цифри!')
+            else:
+                atr = 'number'
+                search_con(query, atr, phonebook)
+        elif search == '5':
+            query = input('Введіть місто: ').strip().capitalize()
+            atr = 'city'
+            search_con(query, atr, phonebook)
+        elif search == '6':
+            continue
     elif operator == '3':
-        query = input('Введіть прізвище: ')
+        query = input('Введіть номер телефону: ').strip()
         for item in phonebook:
-            if query in item['last_name']:
-                print(item)
-                continue
-    elif operator == '4':
-        query = input('Введіть повне ім\'я: ')
-        for item in phonebook:
-            if query in item['full_name']:
-                print(item)
-                continue
-    elif operator == '5':
-        query = input('Введіть номер телефону: ')
-        for item in phonebook:
-            if query == item['number']:
-                print(item)
-                continue
-    elif operator == '6':
-        query = input('Введіть місто: ')
-        for item in phonebook:
-            if query in item['city']:
-                print(item)
-                continue            
-       
-       
-
-    elif operator == '7':
-        query = input('Введіть номер телефону: ')
-     
-        for item in range(len(phonebook)):
-            if(phonebook[item]["number"] == query):
-                del phonebook[item]
-                break
-        with open('phonebook.json', 'w') as filee:
-            json.dump(phonebook, filee, indent=4, ensure_ascii=False)
-        print('Ok')
-
-    elif operator == '8':
-        query = input('Введіть номер телефону: ')
-        for item in phonebook:
-            if query == item['number']:
-                print('Ви дійсно бажаєте оновити дані?')
-                question = input('''
-                Так - 1
-                Ні - 2 \n''')
+            if query in item['number']:
+                print('''Ви дійсно бажаєте видалити дані?
+Так - 1
+Ні - 2 \n''')
+                question = input('Ваш вибір: ')
                 if question == '1':
-                    phonebook.remove(item)
-                    first_name = input('Введіть ім\'я: ').capitalize()
-                    last_name = input('Введіть Прізвище: ').capitalize()
-                    full_name = (f'{first_name} {last_name}')
-                    number = query
-                    city = input('Введіть місто: ').capitalize()
+                    remove(item)
+                    break
+            else:
+                print('Номеру телефону неіснує!')
+                break    
+        
 
+    elif operator == '4':
+        query = input('Введіть номер телефону: ').strip()
+        for item in phonebook:
+            if query == item['number']:
+                print('''Ви дійсно бажаєте оновити дані?
+Так - 1
+Ні - 2 \n''')
+                question = input('Ваш вибір: ')
+                if question == '1': 
+                    first_name = input('Введіть ім\'я: ').strip().capitalize()
+                    last_name = input('Введіть Прізвище: ').strip().capitalize()
+                    full_name = (f'{first_name} {last_name}')
+                    number = query.strip()
+                    city = input('Введіть місто: ').strip().capitalize()
                     new_contact(first_name, last_name, full_name, number, city)
-                else:    
-                    continue    
-       
+                    remove(item)
+            else:
+                print('Номеру телефону неіснує!')    
+                break    
+    
 
     elif operator == 'exit' or operator == 'q':
         print('До побачення!')
         break
-       
+
+      
