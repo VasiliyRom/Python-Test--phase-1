69ca617453a850e1dbc4c5f7f376e132782e2f90
VasiliyRom <63020273+VasiliyRom@users.noreply.github.com>
2020-05-28 11:50:11 +0300

    1

--- homework11.py
+++ homework11.py
@@ -1,20 +1,65 @@
 class Person():
 
-    def first_name(self, *args, **kwargs):
-        pass
-    
-    def last_name():
-        pass
-    def age():
-        pass
+    def __init__(self, name, age, sex, lessons, education, purpose):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.education = education
+        self.purpose = purpose
+        self.lessons = lessons
 
-class Teacher(Person):
-    def salary():
-
-
+    def __str__(self):
+        return f" {self.name.capitalize()} is {self.sex} of {self.age} year old.\n Education level is {self.education}. \nIn shool cause of {self.purpose} and has {self.lessons} a day."
+    def __repr__(self):
+        return f" {self.name.capitalize()} is {self.sex} of {self.age} year old.\n Education level is {self.education}. \nIn shool cause of {self.purpose} and has {self.lessons} a day."
 
 class Student(Person):
 
+    def __init__(self, name, age, sex, lessons, grades, homeworkcize, education = 'school', purpose = 'learning'):
+        super().__init__(name, age, sex, lessons, education, purpose)
+        self.grades = grades
+        self.homeworkcize = homeworkcize
+    
+    def str(self):
+        return f"""
+        {self.name.capitalize()} is {self.sex} of {self.age} year old.
+        Education level is {self.education}.
+        In shool cause of {self.purpose} and has {self.lessons} lessons a day.
+        Marks are {self.grades}.
+        Has {self.homeworkcize} homework
+        """
+    def repr(self):
+        return f"""
+        {self.name.capitalize()} is {self.sex} of {self.age} year old.
+        Education level is {self.education}.
+        In shool cause of {self.purpose} and has {self.lessons} lessons a day.
+        Marks are {self.grades}
+        Has {self.homeworkcize} homework
+        """
+class Teacher(Person):
+
+    def __init__(self, name, age, sex, lessons, sallary, education = 'university', purpose = 'teaching'):
+        super().__init__(name, age, sex, lessons, education, purpose)
+        self.sallary = sallary
+
+    def str(self):
+        return f"""
+        {self.name.capitalize()} is {self.sex} of {self.age} year old.
+        Education level is {self.education}.
+        In shool cause of {self.purpose} and has {self.lessons} lessons a day.
+        Earn {self.sallary} per year"""
+
+    def repr(self):
+        return f"""
+        {self.name.capitalize()} is {self.sex} of {self.age} year old.
+        Education level is {self.education}.
+        In shool cause of {self.purpose} and has {self.lessons} lessons a day.
+        Earn {self.sallary} per year"""
+
+teacher = Teacher('Erica', '28', 'female', '5', '70000')
+print(str(teacher))
+student = Student('Taras', '15', 'male', '7', 'bad', 'not much' )
+print(str(student))
 
 #Task2
 class Test:
--- szsjkrkj.py
+++ szsjkrkj.py
@@ -1,93 +1,62 @@
-class Product:
+class Person():
+
+    def init(self, name, age, sex, lessons, education, purpose):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.education = education
+        self.purpose = purpose
+        self.lessons = lessons
+
+    def str(self):
+        return f" {self.name.capitalize()} is {self.sex} of {self.age} year old.\n Education level is {self.education}. \nIn shool cause of {self.purpose} and has {self.lessons} a day."
+    def repr(self):
+        return f" {self.name.capitalize()} is {self.sex} of {self.age} year old.\n Education level is {self.education}. \nIn shool cause of {self.purpose} and has {self.lessons} a day."
+
+class Student(Person):
+
+    def init(self, name, age, sex, lessons, grades, homeworkcize, education = 'school', purpose = 'learning'):
+        super().__init__(name, age, sex, lessons, education, purpose)
+        self.grades = grades
+        self.homeworkcize = homeworkcize
     
-    prod_type = ""
-    name = ""
-    price = 0
+    def str(self):
+        return f"""
+        {self.name.capitalize()} is {self.sex} of {self.age} year old.
+        Education level is {self.education}.
+        In shool cause of {self.purpose} and has {self.lessons} lessons a day.
+        Marks are {self.grades}.
+        Has {self.homeworkcize} homework
+        """
+    def repr(self):
+        return f"""
+        {self.name.capitalize()} is {self.sex} of {self.age} year old.
+        Education level is {self.education}.
+        In shool cause of {self.purpose} and has {self.lessons} lessons a day.
+        Marks are {self.grades}
+        Has {self.homeworkcize} homework
+        """
+class Teacher(Person):
 
-    def __init__(self, prod_type, name, price):
-        if type(price) != str:
-            self.prod_type = prod_type
-        else:
-            raise TypeError('Prod_typeError')
-        
-        if type(price) != str:
-            self.name = name
-        else:
-            raise TypeError('NameError')
+    def init(self, name, age, sex, lessons, sallary, education = 'university', purpose = 'teaching'):
+        super().__init__(name, age, sex, lessons, education, purpose)
+        self.sallary = sallary
 
-        if type(price) != int or type(price) != float and price > 0:
-            self.price = price
-        else:
-            raise TypeError('PriceError')
+    def str(self):
+        return f"""
+        {self.name.capitalize()} is {self.sex} of {self.age} year old.
+        Education level is {self.education}.
+        In shool cause of {self.purpose} and has {self.lessons} lessons a day.
+        Earn {self.sallary} per year"""
 
-    def __str__(self):
-       return f'{self.name} = {self.price}'
+    def repr(self):
+        return f"""
+        {self.name.capitalize()} is {self.sex} of {self.age} year old.
+        Education level is {self.education}.
+        In shool cause of {self.purpose} and has {self.lessons} lessons a day.
+        Earn {self.sallary} per year"""
 
-    def __repr__(self):
-       return f'{self.name} = {self.price}'
-
-
-class ProductStore:
-    
-    prod_store = []
-    amount = 0
-    profit = 0
-
-    def add(self, product, amount):
-        prod = {}
-        prod["product"] = product
-        prod["amount"] = amount
-        product.price *= 1.3
-        self.prod_store.append(prod)
-
-    def set_discount(self, identifier, percent):
-        if type(percent) == int or type(percent) == float:
-            for i in self.prod_store:
-                if i["product"].prod_type == identifier or i["product"].name == identifier:
-                    i["product"].price = i["product"].price * (1 - (percent / 100))
-        else:
-            raise Exception("Відсотки мають бути числом")
-        
-    def sell_product(self, product_name, amount):
-        for i in self.prod_store:
-            if i['product'].name == product_name:
-                try:
-                    if amount <= i['amount']:
-                        i['amount'] -= amount
-                        self.profit = amount * i['product'].price
-                    else:
-                        raise Exception('Невистачає товару!')
-
-                except Exception as e:
-                    print(e)
-
-    def get_income(self):
-        print(f"{self.profit} UAH")
-
-    def get_all_products(self):
-        print(self.prod_store)
-
-    def get_product_info(self, product_name):
-        for i in self.prod_store:
-            if i['product'].name == product_name:
-                print(product_name, i['amount'])
-
-
-try:
-    p = Product('Sport', 'Football T-Shirt', 100)
-    p2 = Product('Food', 'Ramen', 1.5)
-
-    s = ProductStore()
-
-    s.add(p, 10)
-    s.add(p2, 300)
-    s.add(p, 100000)
-    s.sell_product('Ramen', 10)
-    s.get_income()
-    s.get_all_products()
-    s.get_product_info('Ramen')
-    s.set_discount('Ramen', 10)
-    s.get_all_products()
-
-except ValueError as e:
-    print(e)
+teacher = Teacher('Erica', '28', 'female', '5', '70000')
+#print(str(teacher))
+student = Student('Taras', '15', 'male', '7', 'bad', 'not much' )
+print(str(student))
