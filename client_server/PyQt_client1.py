7e02cd4dc741863973a0a5638e7a4cbee213d09b
VasiliyRom <63020273+VasiliyRom@users.noreply.github.com>
2020-08-12 21:32:14 +0300

    client-server

---
+++ client_server/PyQt_client1.py
@@ -0,0 +1,98 @@
+import sys, asyncio, random
+from quamash import QEventLoop
+from client import EchoClientProtocol
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout, QMainWindow,
+                             QPushButton, QLineEdit, QListWidget)
+
+
+class MyWindow(QMainWindow):
+    def __init__(self, loop, client_new, **kwargs):
+        super(__class__, self).__init__(**kwargs)
+        self.client_new = client_new
+        self.client_new.set_callback(self.output)
+        self.client_new.output = self.output
+        self.loop = loop
+        self.setWindowTitle('Чат')
+        self.setGeometry(200, 200, 300, 450)
+        self.main()
+
+    def output(self, data):
+        self.history.addItem(data)
+
+    def main(self):
+        widget = QWidget()
+        headLayout = QHBoxLayout()
+
+        self.user_name = QLineEdit()
+        self.user_name.setPlaceholderText('Введіть нік')
+        ok_butt = QPushButton('OK')
+
+
+        headLayout.addWidget(self.user_name)
+        headLayout.addWidget(ok_butt)
+
+        self.history = QListWidget()
+        self.editArea = QLineEdit('')
+        self.editArea.setPlaceholderText('Введіть повідомлення')
+        self.editArea.setEnabled(False)
+
+        button = QPushButton('Надіслати')
+
+        mainLayout = QVBoxLayout()
+        mainLayout.addLayout(headLayout)
+        mainLayout.addWidget(self.history)
+        mainLayout.addWidget(self.editArea)
+        mainLayout.addWidget(button)
+
+        widget.setLayout(mainLayout)
+        self.setCentralWidget(widget)
+
+        button.clicked.connect(self.send_message)
+        self.editArea.returnPressed.connect(button.click)
+        ok_butt.clicked.connect(self.check_user)
+
+    def check_user(self):
+        user = self.user_name.text()
+        if user == '':
+            self.user_name.setText(f'User_{int(random.random() * 1000)}')
+        else:
+            self.user_name.setText(user)
+        self.editArea.setEnabled(True)
+        self.user_name.setEnabled(False)
+
+
+    def send_message(self):
+        try:
+            message = self.editArea.text()
+            if message != '':
+                self.client_new.send(message, self.user_name.text())
+            self.editArea.clear()
+        except Exception as e:
+            print(e)
+            print('Error')
+            self.history.addItem('Error')
+
+
+class App(QApplication):
+    def __init__(self):
+        QApplication.__init__(self, sys.argv)
+        self.loop = QEventLoop(self)
+        asyncio.set_event_loop(self.loop)
+        self.userClient = EchoClientProtocol(self.loop, 'user')
+
+        self.loop.create_task(self.start())
+
+        self.window = MyWindow(self.loop, self.userClient)
+        self.window.show()
+        self.loop.run_forever()
+
+
+    async def start(self):
+        print('[START CLIENT]')
+        clientConnection = self.loop.create_connection(lambda: self.userClient, '127.0.0.1', 8888)
+
+        await asyncio.wait_for(clientConnection, 10000, loop=self.loop)
+
+
+if __name__ == '__main__':
+    App()
---
+++ client_server/server.py
@@ -0,0 +1,68 @@
+import asyncio, json, time
+
+
+class EchoServerProtocol(asyncio.Protocol):
+    def __init__(self, clients, messages):
+        self.clients = clients
+        self.msg = messages
+        self.new_client = None
+
+    def connection_made(self, transport):
+        self.peername = transport.get_extra_info('peername')
+        self.transport = transport
+        print(f'З\'єднано з: {self.peername[1]}')
+        if self.transport not in self.clients:
+            print(f'Новий клієнт: {self.peername[1]}')
+            self.new_client = self.transport
+            if len(self.msg) != 0:
+                last_messages = json.dumps({'last_messages': self.msg})
+                self.new_client.write(last_messages.encode())
+
+            self.clients.append(self.new_client)
+            #self.new_client = None
+
+    def data_received(self, data):
+        decode_data = data.decode()
+        json_message = json.loads(decode_data)
+        user_name = json_message.get('user_name')
+        message = json_message.get('message')
+
+        print(f'Отримано меседж: {message} від {user_name}/id{self.peername[1]}')
+        json_messages = json.dumps({'client': user_name, 'message': message})
+        print(f'Відправлено: {message} для {user_name}/id{self.peername[1]}')
+
+        for client in self.clients:
+            client.write(json_messages.encode())
+        self.msg.append(json.loads(json_messages))
+
+
+    def connection_lost(self, exc):
+        print(f'Закрито сокет для: {self.peername[1]}')
+
+        self.transport.close()
+        self.clients.remove(self.transport)
+        message = f'Відєднався: {self.peername[1]}'
+        mess = json.dumps({'client': 'SERVER', 'message': message}).encode()
+
+        for client in self.clients:
+            client.write(mess)
+
+
+
+async def main():
+    # Get a reference to the event loop as we plan to use
+    # low-level APIs.
+    loop = asyncio.get_event_loop()
+    clients = []
+    msg = []
+
+    server = await loop.create_server(lambda: EchoServerProtocol(clients, msg), '127.0.0.1', 8888)
+    print('[START SERVER]')
+    async with server:
+        try:
+            await server.serve_forever()
+        finally:
+            EchoServerProtocol.connection_lost()
+
+if __name__ == '__main__':
+    asyncio.run(main())
